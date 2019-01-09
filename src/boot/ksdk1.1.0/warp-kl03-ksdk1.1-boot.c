4847977efd1a1dc9d27d56911cc2396dddd7c58e
Siegfriedchao <wang_youchao1234@126.com>
2019-01-09 17:09:11 +0000

    a small clean-up

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -2341,8 +2341,9 @@ main(void)
 				SEGGER_RTT_WriteString(0, "\r\n\tSet the time delay between each run in ms(e.g., '1234')> ");
 				uint16_t menuDelayBetweenEachRun = read4digits();
 
-				#ifdef WARP_BUILD_ENABLE_DEVAMG8834
 				SEGGER_RTT_WriteString(0, "\n");
+
+				#ifdef WARP_BUILD_ENABLE_DEVAMG8834
 				for(uint8_t i = 0;i < 64; i++)
 				{
 					SEGGER_RTT_printf(0, " AMG8834 %d,", i);
