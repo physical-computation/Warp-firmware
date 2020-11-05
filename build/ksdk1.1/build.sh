09dd39278943c4c0f6816d5d6a8b8e8f37f93316
stefyk <43543157+stefyk@users.noreply.github.com>
2020-11-05 20:55:21 +0000

    Update build.sh

--- build/ksdk1.1/build.sh
+++ build/ksdk1.1/build.sh
@@ -12,8 +12,8 @@
 	cp ../../src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c		work/demos/Warp/src/
 	cp ../../src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-powermodes.c	work/demos/Warp/src/
 	
-	cp../../src/boot/ksdk1.1.0/devSSD1331.c                         work/demos/Warp/src/
-	cp../../src/boot/ksdk1.1.0/devSSD1331.h  	                work/demos/Warp/src/
+	cp ../../src/boot/ksdk1.1.0/devSSD1331.c                         work/demos/Warp/src/
+	cp ../../src/boot/ksdk1.1.0/devSSD1331.h  	                work/demos/Warp/src/
 	
 	cp ../../src/boot/ksdk1.1.0/warp.h				work/demos/Warp/src/
 	cp ../../src/boot/ksdk1.1.0/devBMX055.*				work/demos/Warp/src/
